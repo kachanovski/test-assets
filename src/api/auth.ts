031e1f02c53304d8d4d4c9d30a0aa38a646db421
kachanovski <kachanovski03@gmail.com>
2021-12-23 18:41:45 +0000

    init commit 5

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,26 +8,26 @@ import axios from "axios";
 export const authAPI = {
   authMe(token?: string) {
     return axios.post(
-      "/api/v1/token/verify/",
+      "http://5.181.108.97/api/v1/token/verify/",
       { token },
       { withCredentials: true }
     );
   },
   login(data: ISignInForm) {
-    return axios.post("/api/v1/cms/login/", data, { withCredentials: true });
+    return axios.post("http://5.181.108.97/api/v1/cms/login/", data, { withCredentials: true });
   },
   resetPassword(data: IResetPasswordForm) {
-    return axios.post("/api/v1/cms/auth/password/reset/", data, {
+    return axios.post("http://5.181.108.97/api/v1/cms/auth/password/reset/", data, {
       withCredentials: true,
     });
   },
   checkResetPasswordToken(uid: string, token: string) {
-    return axios.get(`/api/v1/cms/auth/password/reset/${uid}/${token}/`, {
+    return axios.get(`http://5.181.108.97/api/v1/cms/auth/password/reset/${uid}/${token}/`, {
       withCredentials: true,
     });
   },
   recoveryPassword(data: IRecoveryPasswordForm) {
-    return axios.post(`/api/v1/cms/auth/password/reset/complete/`, data, {
+    return axios.post(`http://5.181.108.97/api/v1/cms/auth/password/reset/complete/`, data, {
       withCredentials: true,
     });
   },
