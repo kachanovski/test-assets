0c99753171e9ca6e40fa748613ebbb9db2f1dc05
kachanovski <kachanovski03@gmail.com>
2021-12-23 18:28:52 +0000

    init commit 2

--- src/pages/users/usersPage.tsx
+++ src/pages/users/usersPage.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from "react";
+import React  from "react";
 import { Users } from "./users";
-import { useDispatch } from "react-redux";
-import { getUsers } from "../../redux/thunks";
+
 
 export const UsersPage = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getUsers());
-  }, []);
   return <Users />;
 };
